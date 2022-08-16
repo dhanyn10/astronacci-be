87f7947760bec291db386181e7d268967f342a72
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-16 19:32:54 +0700

    navbar with session

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,18 @@
 <!-- As a link -->
-<nav class="navbar bg-light">
+<nav class="navbar navbar-expand-md bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+        <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    @if (session()->has('name'))
+                        <a class="nav-link active" aria-current="page" href="{{route('auth-logout')}}">logout</a>
+                    @endif
+                </li>
+            </ul>
+        </div>    
     </div>
-  </nav>
-  
+</nav>
