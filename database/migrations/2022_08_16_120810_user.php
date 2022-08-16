32e9bf0d4536501a0d15b5cd1ba27415fe068ed3
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-17 01:28:06 +0700

    update migration

--- database/migrations/2022_08_16_120810_user.php
+++ database/migrations/2022_08_16_120810_user.php
@@ -16,8 +16,9 @@ return new class extends Migration
         Schema::create('user', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('password')->nullable();
+            $table->integer('oauth')->default(0);
             $table->integer('type')->nullable();
         });
     }
