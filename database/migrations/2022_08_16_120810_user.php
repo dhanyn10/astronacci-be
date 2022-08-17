5af22ee2f611b44cef0309f75803e3bb8b301d5f
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-17 13:12:48 +0700

    add role column

--- database/migrations/2022_08_16_120810_user.php
+++ database/migrations/2022_08_16_120810_user.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->smallInteger('role')->default(0);
             $table->string('password')->nullable();
             $table->smallInteger('oauth')->default(0);
         });
