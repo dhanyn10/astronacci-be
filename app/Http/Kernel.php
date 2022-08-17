780af22b4fa83643eb2f6f440175c074d6155916
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-17 18:34:16 +0700

    register admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
 
         //custom middleware
         'user' => \App\Http\Middleware\UserMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
