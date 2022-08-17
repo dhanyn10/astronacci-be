d6cb94a4124836a76317d61843a3f9d26c992e93
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-17 18:37:12 +0700

    display admin

---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            @include('sidebar')
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    @yield('admin-content')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\http\Controllers\LoginController;
 use App\http\Controllers\LogoutController;
 use App\http\Controllers\HomeController;
 use App\http\Controllers\SettingsController;
+use App\http\Controllers\AdminController;
 
 Route::group([
     'prefix'        => 'auth',
@@ -23,6 +24,15 @@ Route::group([
     Route::post('register', [RegisterController::class, 'register']);
     Route::post('login', [LoginController::class, 'login']);
 });
+
+Route::group([
+    'middleware'    => 'admin',
+    'as'            => 'admin-',
+    'prefix'        => 'admin'
+], function () {
+    Route::get('/', [AdminController::class, 'show'])->name('home');
+});
+
 Route::group([
     'middleware'    => 'user'
 ], function () {
